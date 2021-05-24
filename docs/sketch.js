0e15acc96db902215bb34a24b3e2e1fba3acdf7a
June YAMAMOTO <june@yambal.net>
2021-05-25 01:44:42 +0900

    capture

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,47 +1,28 @@
-let mic;
-
-function setup() {
-  let cnv = createCanvas(710, 200);
-  cnv.mousePressed(userStartAudio);
-  mic = new p5.AudioIn();
-  mic.start();
-}
-
-function draw() {
-  background(200);
-
-  if(mic) {
-     
-  
-    // Get the overall volume (between 0 and 1.0)
-    let vol = mic.getLevel();
-    fill(127);
-    stroke(0);
-
-    // Draw an ellipse with height based on volume
-    let h = map(vol, 0, 1, height, 0);
-    ellipse(width / 2, h - 25, 50, 50);
-  }
-}
-
-/* https://editor.p5js.org/kaori/sketches/nZuHBaT2 */
-
 /*
+080_ウェブカメラの映像を表示する
+*/
+
+//ウェブカメラが接続されているPCやスマートフォンで
+//カメラからの映像を利用できる
+
+let capture;
+
 function setup() {
-  // mimics the autoplay policy
-  getAudioContext().suspend();
+  createCanvas(640, 480);
 
-  let mySynth = new p5.MonoSynth();
-
-  // This won't play until the context has resumed
-  mySynth.play('A6');
+  //ビデオデバイスの読み込み．詳細な設定はリファレンスを参照
+  //https://p5js.org/reference/#/p5/createCapture
+  capture = createCapture(VIDEO);
+  capture.hide();
 }
+
 function draw() {
   background(220);
-  textAlign(CENTER, CENTER);
-  text(getAudioContext().state, width/2, height/2);
+
+  //カメラからの映像はcaptureで利用できるが一旦getしてイメージとして扱うほうが軽量
+  let img = capture.get();
+
+  //これ以降は画像の再構成の作例などを参照し，点描などが可能
+
+  image(img, 0, 0);
 }
-function mousePressed() {
-  userStartAudio();
-}
-*/
