495d717e3c2a813e5c79f57a8d2e8557a68cb412
June YAMAMOTO <june@yambal.net>
2021-05-25 00:45:07 +0900

    mic

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,10 +1,13 @@
 var mic;
+
 function setup(){
   mic = new p5.AudioIn()
   mic.start();
 }
+
 function draw(){
   background(0);
   micLevel = mic.getLevel();
+  console.log(micLevel)
   ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
 }
