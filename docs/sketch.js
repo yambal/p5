6ce305f9477ec3a8c7679e0ed339fc6bc6338786
June YAMAMOTO <june@yambal.net>
2021-05-25 02:03:14 +0900

    video

--- docs/sketch.js
+++ docs/sketch.js
@@ -6,6 +6,7 @@
 //カメラからの映像を利用できる
 
 let capture;
+const images = []
 
 function setup() {
   createCanvas(640, 480);
@@ -21,6 +22,14 @@ function draw() {
 
   //カメラからの映像はcaptureで利用できるが一旦getしてイメージとして扱うほうが軽量
   let img = capture.get();
+  images.push(img)
+  if(images.length > 100){
+    const firstImg = images.shift()
+    for(i = 0; i < 100; i++) {
+      const cImg = images[i]
+      image(img, 0, i * 10);
+    }
+  }
 
   //これ以降は画像の再構成の作例などを参照し，点描などが可能
 
