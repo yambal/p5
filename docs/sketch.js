ec7668cadcb01afd81a79372d84cf91f70f0bf66
June YAMAMOTO <june@yambal.net>
2021-05-25 00:07:08 +0900

    AudioIn

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,31 +1,10 @@
-var mic
-var vol = 0
-
-function setup() {
-  var myDiv = createDiv('click to start audio');
-  myDiv.position(0, 0);
-
-  var mySynth = new p5.MonoSynth();
-
-  // This won't play until the context has started
-  mySynth.play('A6');
-
-  // Start the audio context on a click/touch event
-  userStartAudio().then(function() {
-     myDiv.remove();
-   });
+var mic;
+function setup(){
+  mic = new p5.AudioIn()
+  mic.start();
 }
-
-function draw() {
-  background(200);
-
-  // Get the overall volume (between 0 and 1.0)
-   var vol = mic.getLevel();
-  
-  fill(127);
-  stroke(0);
-  
-  // Draw an ellipse with height based on volume
-  var h = map(vol, 0, 0.5, height, 0);
-  ellipse(width/2, h - 25, 50, 50);
+function draw(){
+  background(0);
+  micLevel = mic.getLevel();
+  ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
 }
